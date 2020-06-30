950c507894bcf234cef78b8952cbb3b6abf318b4
alexgw <alex@Alexs-MacBook-Air.local>
2020-06-30 21:18:02 +0100

    Styling and gui tweaks

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   vals = new Values();
-  let gui = new dat.GUI();
+  let gui = new dat.GUI( { width: 350 } );;
 
 
 
@@ -13,6 +13,8 @@ function setup() {
   f1.addColor(vals, 'stroke_colour');
   f1.addColor(vals, 'shape_fill');
 
+  f1.open()
+
   gui.add(vals, 'shape1',{ forward_slash: 0, vertical_line: 1, circle: 2, square: 3, dot: 4})
   gui.add(vals, 'shape2',{ backward_slash: 0, horizontal_line: 1, circle: 2, square: 3, dot: 4 })
  
@@ -21,32 +23,45 @@ function setup() {
   f2.add(vals, 'shapes_filled');
   f2.add(vals, 'shape_stroke_off');
 
+  f2.open()
+
   var f3 = gui.addFolder('randomise! - here be dragons');
-  f3.add(vals, 'randomise_spacing_per_shape');
-  f3.add(vals, 'random_spacing_per_shape_range', 1, 200);
-  f3.add(vals, 'randomise_weight_per_shape');
-  f3.add(vals, 'random_stroke_per_shape_range', 1, 200);
-  f3.add(vals, 'randomise_spacing_per_row');
-  f3.add(vals, 'random_spacing_per_row_range', 1, 200);
+  f3.add(vals, 'r_spacing_per_shape');
+  f3.add(vals, 'r_spacing_range', 1, 200);
+  f3.add(vals, 'r_spacing_centre', 1, 200);
+  f3.add(vals, 'r_weight_per_shape');
+  f3.add(vals, 'r_stroke_range', 1, 200);
+  f3.add(vals, 'r_stroke_centre', 1, 200);
+  f3.add(vals, 'r_spacing_per_row');
+  f3.add(vals, 'r_spacing_row_range', 1, 200);
+  f3.add(vals, 'r_spacing_row_centre', 1, 200);
+
+  //gui.add(vals, 'run')
 
 
-
-  createCanvas(600, 600)
+  cnv = createCanvas(600, 600);
+  centerCanvas();
  
   button = createButton('Run!');
-  button.position(610, 20);
+  button.addClass('button');
+  //button.position(, 610);
+
   button.style('font-size', '30px');
-  let col = color('#eb2fc8')
+  let col = color('#038cfc')
   button.style('background-color', col);
-    
+  button.position(cnv.x+260,cnv.y-50)  
 }
 
 //the draw function runs every frame, 60 times a second
 function draw() {
+
+   let running = false
   //ten_print(spacing)
 noLoop();
  button.mousePressed(loop);
   
+  if(running == true){print("true")}
+
   Ten_print(
     vals.x,
     vals.y,
@@ -61,16 +76,21 @@ noLoop();
     vals.shape1,
     vals.shape2,
     vals.shape_stroke_off,
-    vals.randomise_spacing_per_shape,
-    vals.random_spacing_per_shape_range,
-    vals.randomise_weight_per_shape,
-    vals.random_stroke_per_shape_range,
-    vals.randomise_spacing_per_row,
-    vals.random_spacing_per_row_range
+    vals.r_spacing_per_shape,
+    vals.r_spacing_range,
+    vals.r_spacing_centre,
+    vals.r_weight_per_shape,
+    vals.r_stroke_range,
+    vals.r_stroke_centre,
+    vals.r_spacing_per_row,
+    vals.r_spacing_row_range,
+    vals.r_spacing_row_centre,
     )
 }
 
 function Values(){
+
+ 
 
   this.x = 0
   this.y = 0
@@ -96,21 +116,57 @@ function Values(){
 
   this.shape_stroke_off = false
 
-  this.randomise_spacing_per_shape = false
+  this.r_spacing_per_shape = false
 
-  this.random_spacing_per_shape_range = 50
+  this.r_spacing_range = 50
 
-  this.randomise_weight_per_shape = false
+  this.r_weight_per_shape = false
 
-  this.random_stroke_per_shape_range = 10
+  this.r_stroke_range = 10
 
-  this.randomise_spacing_per_row = false
+  this.r_spacing_per_row = false
 
-  this.random_spacing_per_row_range = 50
+  this.r_spacing_row_range = 50
+
+  this.r_stroke_centre = 7
+
+  this.r_spacing_centre = 30
+
+  this.r_spacing_row_centre = 30
+
+  this.run = function(){
+  
+
+  }
 }
 
 
-function Ten_print(x,y,spacing,stroke_Weight,bg_colour,stroke_colour,shape_fill,shapes_filled,stroke_caps,chance, shape1, shape2,stroke_off, randomise_spacing_per_shape,random_spacing_per_shape_range,randomise_weight_per_shape,random_stroke_per_shape_range,randomise_spacing_per_row,random_spacing_per_row_range){
+
+
+function Ten_print(
+  x,
+  y,
+  spacing,
+  stroke_Weight,
+  bg_colour,
+  stroke_colour,
+  shape_fill,
+  shapes_filled,
+  stroke_caps,
+  chance, 
+  shape1, 
+  shape2,
+  stroke_off, 
+  r_spacing_per_shape,
+  r_spacing_range,
+  r_spacing_centre,
+  r_weight_per_shape,
+  r_stroke_range,
+  r_stroke_centre,
+  r_spacing_per_row,
+  r_spacing_row_range,
+  r_spacing_row_centre
+  ){
 
   clear()
 
@@ -198,12 +254,12 @@ function Ten_print(x,y,spacing,stroke_Weight,bg_colour,stroke_colour,shape_fill,
     x = x + spacing;
 
   
-    if(randomise_spacing_per_shape){
-    spacing = random(random_spacing_per_shape_range)
+    if(r_spacing_per_shape){
+    spacing = randomGaussian(r_spacing_centre,r_spacing_range)
     }
 
-    if(randomise_weight_per_shape){
-    strokeWeight(random(random_stroke_per_shape_range))
+    if(r_weight_per_shape){
+    strokeWeight(randomGaussian(r_stroke_centre,r_stroke_range))
     }
     
 
@@ -211,13 +267,26 @@ function Ten_print(x,y,spacing,stroke_Weight,bg_colour,stroke_colour,shape_fill,
     if (x > width) {
       x = 0;
       y = y + spacing;
-      if(randomise_spacing_per_row){
-      spacing = random(random_spacing_per_row_range)+10
+      if(r_spacing_per_row){
+      spacing = randomGaussian(r_spacing_row_centre,r_spacing_row_range)+10
       }
     }
 
  
   }
   
+
+}
+
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
+  button.position(cnv.x+260,cnv.y-50)
 
 }
